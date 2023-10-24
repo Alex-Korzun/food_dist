d7b3e1ec84e8e9c3ea515625eb0a80ded612c329
Alex Korzun <koleksandr4@gmail.com>
2023-10-24 19:58:35 +0300

    refactor code

--- js/script.js
+++ js/script.js
@@ -157,26 +157,47 @@ class MenuItem {
         this.itemSubtitle = itemSubtitle;
         this.itemDescription = itemDescription;
         this.itemTotal = itemTotal;
+        this.transfer = 36;
+        this.changeToUAH();
     }
+
     buildMenu() {
         menu.innerHTML += `
-        <div class="menu__item">
-            <img src="${this.source}" alt="${this.alt}">
-            <h3 class="menu__item-subtitle">${this.itemSubtitle}</h3>
-            <div class="menu__item-descr">${this.itemDescription}</div>
-            <div class="menu__item-divider"></div>
-            <div class="menu__item-price">
-                <div class="menu__item-cost">Цена:</div>
-                <div class="menu__item-total"><span>${this.itemTotal}</span> грн/день</div>
-            </div>
-        </div>`;
+            <div class="menu__item">
+                <img src="${this.source}" alt="${this.alt}">
+                <h3 class="menu__item-subtitle">${this.itemSubtitle}</h3>
+                <div class="menu__item-descr">${this.itemDescription}</div>
+                <div class="menu__item-divider"></div>
+                <div class="menu__item-price">
+                    <div class="menu__item-cost">Цена:</div>
+                    <div class="menu__item-total"><span>${this.itemTotal}</span> грн/день</div>
+                </div>
+            </div>`;
+    }
+
+    changeToUAH() {
+        this.itemTotal *= this.transfer;
     }
 }
 
-const fitnessItem = new MenuItem('img/tabs/vegy.jpg', 'vegy', 'Меню "Фитнес"', 'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!', '229');
-const premiumItem = new MenuItem('img/tabs/elite.jpg', 'elite', 'Меню “Премиум”', 'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!', '550');
-const leanItem = new MenuItem('img/tabs/post.jpg', 'post', 'Меню "Постное"', 'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.', '430');
+new MenuItem(
+    'img/tabs/vegy.jpg',
+    'vegy', 
+    'Меню "Фитнес"', 'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
+    6
+    ).buildMenu();
 
-fitnessItem.buildMenu();
-premiumItem.buildMenu();
-leanItem.buildMenu();
+new MenuItem(
+    'img/tabs/elite.jpg',
+    'elite',
+    'Меню “Премиум”', 'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
+    15
+    ).buildMenu();
+
+new MenuItem(
+    'img/tabs/post.jpg',
+    'post',
+    'Меню "Постное"',
+    'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
+    12
+    ).buildMenu();
