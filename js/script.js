4a9efc81a1559d48708e7afa99b2aecab0de37ef
Alex Korzun <koleksandr4@gmail.com>
2023-11-09 16:56:31 +0200

    add navigation bar to slider, refactor old code

--- js/script.js
+++ js/script.js
@@ -293,7 +293,7 @@ window.addEventListener('DOMContentLoaded', () => {
         offset = 0;
 
     totalSlides.innerHTML = images.length < 10 ? `0${images.length}` : images.length;
-    currentSlide.innerHTML = currentSlideIndex < 10 ? `0${currentSlideIndex}` : currentSlideIndex;
+    addZeroToNumber(currentSlide, currentSlideIndex);
 
     sliderField.style.width = 100 * images.length + '%';
     sliderField.style.display = 'flex';
@@ -305,20 +305,66 @@ window.addEventListener('DOMContentLoaded', () => {
         image.style.width = width;
     });
 
+    slider.style.position = 'relative';
+
+    const indicators = document.createElement('ol'),
+        dots = [];
+    indicators.classList.add('slider-indicators');
+    indicators.style.cssText = `
+        position: absolute;
+        right: 0;
+        bottom: 0;
+        left: 0;
+        z-index: 15;
+        display: flex;
+        justify-content: center;
+        margin-right: 15%;
+        margin-left: 15%;
+        list-style: none;
+    `;
+    slider.append(indicators);
+
+    for (let i = 0; i < images.length; i++) {
+        const dot = document.createElement('li');
+
+        dot.setAttribute('data-slide-to', i + 1);
+        dot.style.cssText = `
+            box-sizing: content-box;
+            flex: 0 1 auto;
+            width: 30px;
+            height: 6px;
+            margin-right: 3px;
+            margin-left: 3px;
+            cursor: pointer;
+            background-color: #fff;
+            background-clip: padding-box;
+            border-top: 10px solid transparent;
+            border-bottom: 10px solid transparent;
+            opacity: .5;
+            transition: opacity .6s ease;
+        `;
+        if (i == 0) {
+            dot.style.opacity = 1;
+        }
+        indicators.append(dot);
+        dots.push(dot);
+    }
+
     sliderNextBtn.addEventListener('click', () => {
         if (offset == +width.slice(0, width.length - 2) * (images.length - 1)) {
             offset = 0;
         } else {
             offset += +width.slice(0, width.length - 2);
         }
-        sliderField.style.transform = `translateX(-${offset}px)`;
+        switchOffset(sliderField, offset);
 
         if (currentSlideIndex === images.length) {
             currentSlideIndex = 1;
         } else {
             currentSlideIndex++;
         }
-        currentSlide.innerHTML = currentSlideIndex < 10 ? `0${currentSlideIndex}` : currentSlideIndex;
+        addZeroToNumber(currentSlide, currentSlideIndex);
+        switchDots(dots, currentSlideIndex);
     });
 
     sliderPrevBtn.addEventListener('click', () => {
@@ -327,13 +373,40 @@ window.addEventListener('DOMContentLoaded', () => {
         } else {
             offset -= +width.slice(0, width.length - 2);
         }
-        sliderField.style.transform = `translateX(-${offset}px)`;
+        switchOffset(sliderField, offset);
 
         if (currentSlideIndex === 1) {
             currentSlideIndex = images.length;
         } else {
             currentSlideIndex--;
         }
-        currentSlide.innerHTML = currentSlideIndex < 10 ? `0${currentSlideIndex}` : currentSlideIndex;
+        addZeroToNumber(currentSlide, currentSlideIndex);
+        switchDots(dots, currentSlideIndex);
     });
+
+    dots.forEach(dot => {
+        dot.addEventListener('click', (e) => {
+            const slideTo = +e.target.getAttribute('data-slide-to');
+
+            currentSlideIndex = slideTo;
+            offset = +width.slice(0, width.length - 2) * (slideTo - 1);
+
+            switchOffset(sliderField, offset);
+            addZeroToNumber(currentSlide, currentSlideIndex);
+            switchDots(dots, currentSlideIndex);
+        });
+    });
+
+    function addZeroToNumber(slide, index) {
+        slide.innerHTML = index < 10 ? `0${index}` : index;
+    }
+
+    function switchDots(arr, index) {
+        arr.forEach(dot => dot.style.opacity = '.5');
+        arr[index - 1].style.opacity = 1;
+    }
+
+    function switchOffset(sliderField, offset) {
+        sliderField.style.transform = `translateX(-${offset}px)`;
+    }
 });
